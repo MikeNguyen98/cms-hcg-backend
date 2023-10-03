ded5c71ef310f7fbbddb4d2583cf00e5f7ee1dfa
cuongnm <cuongnm>
2023-10-03 16:57:59 +0700

    s

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as redisStore from 'cache-manager-redis-store';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
@@ -17,22 +17,21 @@ import { UsersModule } from './modules/users/users.module';
       port: 6379, //default port
       ttl: 2000 //ttl
     }),
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configService: ConfigService) => {
-    //     // const username = configService.get('MONGO_USERNAME');
-    //     // const password = configService.get('MONGO_PASSWORD');
-    //     const database = configService.get('MONGO_DATABASE');
-    //     // const host = configService.get('MONGO_HOST');
-
-    //     return {
-    //       // uri: `mongodb://${username}:${password}@${host}`,
-    //       uri: `mongodb://localhost:27017`,
-    //       dbName: database
-    //     };
-    //   },
-    //   inject: [ConfigService]
-    // }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+        return {
+          uri: `mongodb+srv://${username}:${password}@${host}`,
+          // uri: `mongodb://localhost:27017`,
+          dbName: database
+        };
+      },
+      inject: [ConfigService]
+    }),
     AuthModule,
     UsersModule
   ],
