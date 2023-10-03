01684c2e3dc088c14708404305c0a92440bd0f2e
cuongnm <cuongnm>
2023-10-03 16:07:47 +0700

    3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  get(): string {
+    return 'success';
+  }
   @Get('/favicon.ico')
   getHello(): string {
     return 'success';
