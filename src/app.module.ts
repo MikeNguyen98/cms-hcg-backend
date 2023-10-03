d809caa20b1d8da812d7f74e4412e1514f4648ea
cuongnm <cuongnm>
2023-10-03 16:34:26 +0700

    ss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import * as redisStore from 'cache-manager-redis-store';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 
@@ -19,26 +17,24 @@ import { UsersModule } from './modules/users/users.module';
       port: 6379, //default port
       ttl: 2000 //ttl
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        // const username = configService.get('MONGO_USERNAME');
-        // const password = configService.get('MONGO_PASSWORD');
-        const database = configService.get('MONGO_DATABASE');
-        // const host = configService.get('MONGO_HOST');
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => {
+    //     // const username = configService.get('MONGO_USERNAME');
+    //     // const password = configService.get('MONGO_PASSWORD');
+    //     const database = configService.get('MONGO_DATABASE');
+    //     // const host = configService.get('MONGO_HOST');
 
-        return {
-          // uri: `mongodb://${username}:${password}@${host}`,
-          uri: `mongodb://localhost:27017`,
-          dbName: database
-        };
-      },
-      inject: [ConfigService]
-    }),
+    //     return {
+    //       // uri: `mongodb://${username}:${password}@${host}`,
+    //       uri: `mongodb://localhost:27017`,
+    //       dbName: database
+    //     };
+    //   },
+    //   inject: [ConfigService]
+    // }),
     AuthModule,
     UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService]
 })
 export class AppModule {}
