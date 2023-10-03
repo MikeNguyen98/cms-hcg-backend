de65002d9b17e456806a40498883e3d53cdf2d45
cuongnm <cuongnm>
2023-10-03 16:14:43 +0700

    ss

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   app.use(
     session({
       secret: 'keyboard',
