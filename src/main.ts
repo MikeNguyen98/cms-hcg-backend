d7100e8a8ee2aeb41587dead1e372ae9566598bd
cuongnm <cuongnm>
2023-10-03 17:42:10 +0700

    s

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
-  await app.listen(8080);
+  await app.listen(3000);
 }
 bootstrap();
